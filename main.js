25edfb0e7e567bca97eb5fecdf7b5c77a3c84530
imane <bellaghma.imane@gmail.com>
2024-06-29 15:02:23 +0100

    add

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 // main.js File
 import calc from "./mod-one.js";
 
-import * as modOne from "./mod-two.js"
+import * as modOne from "./mod-two.js";
 console.log(calc(modOne.numOne, modOne.numTwo, modOne.numThree)); // 60
-
-// mod-two.js File
-let a = 10; // Do Not Edit Names
-let b = 20; // Do Not Edit Names
-let c = 30; // Do Not Edit Names
